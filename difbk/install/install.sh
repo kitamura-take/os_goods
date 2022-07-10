d8bc61eb92ebd333b41f3d19233bfa6fa089d152
kitamura-take <hoperstep@gmail.com>
2022-07-10 23:55:54 +0900

    chmod & difbk installscript & cmdclick bugfix

---
+++ difbk/install/install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+CURRENT_DIR_PATH="$(echo $(cd $(dirname $0) && pwd))"
+input_os_type="${1}"
+declare -A OS_TYPE_MAPS=(	
+	["l"]="linux"
+	["m"]="mac"
+	["w"]="win"
+)
+execute_installer(){ bash "${CURRENT_DIR_PATH}/${1}/installer.sh" ;}
+case "${input_os_type}" in 
+	"")
+		while true	
+		do
+			echo -e "${OS_TYPE_MAPS["l"]}\n${OS_TYPE_MAPS["m"]}\n${OS_TYPE_MAPS["w"]}"
+			read -ep "type your os type in above :> " input_os_type 
+			case "${input_os_type}" in 
+				"${OS_TYPE_MAPS["l"]}"|"${OS_TYPE_MAPS["m"]}"|"${OS_TYPE_MAPS["w"]}") break ;;
+				*) ;; esac
+		done ;; esac 
+execute_installer "${input_os_type}"
+	
